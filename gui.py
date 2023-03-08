d484235f72d0d884f4c44d788a4ead490d767334
boompires <bman721@gmail.com>
2023-03-08 10:56:45 -0500

    Added a complete and exit button to app

--- gui.py
+++ gui.py
@@ -7,15 +7,21 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=funcs.get_todos(), key="todos",
                       enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+
+window_layout = [[label],
+                 [input_box, add_button],
+                 [list_box, edit_button, complete_button],
+                 [exit_button]]
 
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout= window_layout,
                    font=('Helvetica', 20))
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+
     match event:
         case "Add":
             todos = funcs.get_todos()
@@ -31,6 +37,15 @@ while True:
             todos[index] = new_todo + "\n"
             funcs.write_todos(todos)
             window['todos'].update(values=todos)
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = funcs.get_todos()
+            todos.remove(todo_to_complete)
+            funcs.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")
+        case "Exit":
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
